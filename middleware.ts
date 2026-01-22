2ee3047b7115812c684c1c36e0f64823d025a986
t10tbcai9898 <deeak57474@gmail.com>
2026-01-22 23:57:24 +0530

    Update middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -1,17 +1,1 @@
-import { updateSession } from "@/lib/supabase/middleware"
-i// Source - https://stackoverflow.com/q
-// Posted by rolias4031
-// Retrieved 2026-01-22, License - CC BY-SA 4.0
 
-import { NextResponse } from 'next/server';
-
-export default function middleware(request) {
-  return NextResponse.next();
-}
-
-
-export const config = {
-  matcher: [
-    "/((?!_next/static|_next/image|favicon.ico|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)",
-  ],
-}
